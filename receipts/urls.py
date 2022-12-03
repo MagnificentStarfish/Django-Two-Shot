27f533494395196c7f5648c0b07dbbfcf7e97dda
Donald <Djp0242@gmail.com>
2022-12-02 16:47:15 -0800

    Feature 6 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from receipts.views import receipt_view
 
-urlpatterns = [
-    path("", receipt_view, name="home")
-]
+urlpatterns = [path("", receipt_view, name="home")]
